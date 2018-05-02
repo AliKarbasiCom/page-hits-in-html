b1d5ebc779bcf7162512b6e8c2b76180afeb2c2c
Ali Karbasi <iam@alikarbasi.com>
2018-05-02 18:49:30 +0430

    hits.php
    The PHP file has a file " counter.txt " that stores the number of times, the page has been accessed. It increases a count by one.
    Add this script in <body> tag to start counting :
    <script type="text/javascript" src="hits.php"></script>

---
+++ hits.php
@@ -0,0 +1,9 @@
+<?php
+    $counts = ("counter.txt");
+    $hits = file($counts);
+    $hits[0] ++;
+    $fp = fopen($counts, "w");
+    fputs($fp , "$hits[0]");
+    fclose($fp);
+    header("Content-type: text/javascript");
+?>
